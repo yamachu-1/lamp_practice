d41118158345c6e02d4f771da065997416140a45
ゆうじ <p13163t0yss1td@gmail.com>
2020-09-27 14:41:23 +0900

    課題１のhtmlspecialchar設定をしました。

--- www/view/login_view.php
+++ www/view/login_view.php
@@ -22,6 +22,7 @@
         <input type="password" name="password" id="password" class="form-control">
       </div>
       <input type="submit" value="ログイン" class="btn btn-primary">
+      <input type="hidden" name="csrf_token" value="<?php get_csrf_token();?>">
     </form>
   </div>
 </body>
